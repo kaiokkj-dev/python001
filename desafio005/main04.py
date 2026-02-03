3ba40dea50aa5899dfd7542b29c34c25951206ee
kaiokkj-dev <kaiohenriquemalaquias@gmail.com>
2026-02-03 02:55:40 -0300

    novo

--- desafio005/main04.py
+++ desafio005/main04.py
@@ -1,4 +1,4 @@
 k = str(input('Digite uma frase: ')).strip()
-print('A letra A aparece {} vezes  na frase.'.format(k.upper().count('A')))
-print('A primeira letra A aparece na posicao {}'.format(k.upper().find('A') + 1))
-print('A ultima letra A aparece na posicao {}'.format(k.upper().rfind('A') + 1))
+print('A letra A aparece {} vezes  na frase.'.format(k.upper().count('A'))) # count conta quantas vezes aparece a letra
+print('A primeira letra A aparece na posicao {}'.format(k.upper().find('A') + 1)) # find procura a primeira ocorrencia da letra
+print('A ultima letra A aparece na posicao {}'.format(k.upper().rfind('A') + 1)) # rfind procura da direita para a esquerda
--- desafio005/main05.py
+++ desafio005/main05.py
@@ -1,4 +1,4 @@
 k = str(input('Digite seu nome completo: ')).strip() 
 print('Muito prazer em te conhecer!')
-print('Seu primeiro nome e {}'.format(k.split()[0]))
-print('Seu ultimo nome e {}'.format(k.split()[-1]))
+print('Seu primeiro nome e {}'.format(k.split()[0])) # split() divide a string em uma lista
+print('Seu ultimo nome e {}'.format(k.split()[-1])) # -1 pega a ultima posicao da lista
